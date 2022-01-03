42d0532fd7081097bfc78909c6bd6814c1ab38cb
alessio <alessioguardo9@hotmail.it>
2022-01-03 11:39:15 +0100

    Initial commit

---
+++ src/main/java/com/unict/fghotel/FghotelApplication.java
@@ -0,0 +1,13 @@
+package com.unict.fghotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FghotelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FghotelApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/unict/fghotel/FghotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unict.fghotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FghotelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
