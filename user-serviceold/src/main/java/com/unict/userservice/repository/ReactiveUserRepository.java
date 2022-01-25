6dbf82be59df5aea44df762e146de39c88be9eb3
massimo <massimo.falzone@hotmail.it>
2022-01-25 17:28:21 +0100

    update readme + pulizia parziale codice

--- user-serviceold/src/main/java/com/unict/userservice/repository/ReactiveUserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.unict.userservice.repository;
-
-import com.unict.userservice.model.User;
-import org.bson.types.ObjectId;
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
-
-public interface ReactiveUserRepository extends ReactiveCrudRepository<User, ObjectId> {
-}
